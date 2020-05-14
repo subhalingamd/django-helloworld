fd3718cf1b61f80143fb56a38797c9c937bde6fb
Subhalingam D <subhalingam.d@gmail.com>
2020-05-14 13:11:16 +0530

    tut 3 (skipped namespaces)

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,41 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 from django.http import HttpResponse
+from django.http import Http404
+
+from .models import Question
+
+
 
 def index(request):
-	return HttpResponse("Hello World")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	
+	context = {'latest_question_list': latest_question_list}
+	return render(request, 'polls/index.html', context)
+
+	#output = ', '.join([q.question_text for q in latest_question_list])
+	return HttpResponse(output)
+
+	#return HttpResponse("Hello World")
+
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question': question})
+
+	'''
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist")
+	return render(request, 'polls/detail.html', {'question': question})
+    '''
+
+    #return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
